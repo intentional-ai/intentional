3d39497745472c65505d5eee87cc229bd7ba5c3c
ZanSara <github@zansara.dev>
2024-10-19 11:59:43 +0100

    isolate tests

--- .pre-commit-pylint.sh
+++ .pre-commit-pylint.sh
@@ -8,24 +8,28 @@ run_pylint() {
     if [[ -d "$dir" ]]; then
       # Run pylint in the second-level directory
       echo "Running pylint in $dir"
-      pylint -sn -rn --rcfile=$dir/pyproject.toml "$dir"
+      pylint -sn -rn --rcfile=$dir/pyproject.toml "$dir" || exit 1
     fi
   done
 }
 
-
-
 if [[ $1 == "all" ]]; then
   # If the first argument is 'all', find all the folders
+  echo "Running pylint in ./intentional"
+  pylint -sn -rn --rcfile=intentional/pyproject.toml intentional || exit 1
   echo "Running pylint in ./intentional-core"
-  pylint -sn -rn --rcfile=intentional-core/pyproject.toml intentional-core
+  pylint -sn -rn --rcfile=intentional-core/pyproject.toml intentional-core || exit 1
   cd plugins/
   all_dirs=$(find . -type d | grep '^\./intentional' | grep '/src/' | grep -v ".egg-info" | awk -F'/' '{print $1"/"$2}' | sort -u)
   run_pylint "$all_dirs"
+
 else
   # Get the list of changed files and extract the directory they're in
   changed_dirs=$(git diff --name-only main...HEAD && git diff --name-only --cached | grep '^intentional' | awk -F'/' '{print $1}' | sort -u)
+  echo "Affected main directories: $changed_dirs"
   run_pylint "$changed_dirs"
+
+  echo "Affected plugin directories: $changed_dirs"
   changed_dirs=$(git diff --name-only main...HEAD && git diff --name-only --cached | grep '^plugins/intentional' | awk -F'/' '{print $1"/"$2}' | sort -u)
   run_pylint "$changed_dirs"
 fi
--- .pre-commit-pytest.sh
+++ .pre-commit-pytest.sh
@@ -8,22 +8,32 @@ run_pytest() {
     if [[ -d "$dir" && "$dir" == intentional* ]]; then
       # Navigate to the directory, run pytest, then return to the original directory
       echo "Running pytest in $dir"
-      (cd "$dir" && pytest)
+      pytest $dir || exit 1
     fi
   done
 }
 
 
-echo "Running pytest in intentional-core/"
-pytest intentional-core/
-cd plugins
-
 if [[ $1 == "all" ]]; then
+  # If the first argument is 'all', find all the folders
+  echo "Running pytest in ./intentional"
+  pytest intentional || exit 1
+  echo "Running pytest in ./intentional-core"
+  pytest intentional-core || exit 1
+  cd plugins/
+
   # If argument is 'all', find all directories starting with 'intentional'
   all_dirs=$(find . -type d -name 'intentional*' -printf '%f\n' | sort -u)
   run_pytest "$all_dirs"
+
 else
-  # Otherwise, get the list of changed directories starting with 'intentional'
+  # Get the list of changed files and extract the directory they're in
   changed_dirs=$(git diff --name-only main...HEAD && git diff --name-only --cached | grep '^intentional' | awk -F'/' '{print $1}' | sort -u)
+  echo "Affected main directories: $changed_dirs"
+  run_pytest "$changed_dirs"
+
+  # Otherwise, get the list of changed directories starting with 'intentional'
+  changed_dirs=$(git diff --name-only main...HEAD && git diff --name-only --cached | grep '^plugins/intentional' | awk -F'/' '{print $2}' | sort -u)
+  echo "Affected plugins directories: $changed_dirs"
   run_pytest "$changed_dirs"
 fi
