23da0c90b012991c6c6eb8cd5bd60499684f1dc0
ZanSara <github@zansara.dev>
2024-10-20 18:29:27 +0100

    easier way to check if we are on main

--- .github/workflows/scripts/get_affected_folders.sh
+++ .github/workflows/scripts/get_affected_folders.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-if git diff --quiet origin/main HEAD; then
+if if [ "$(git branch --show-current)" = "main" ]; then
+    # We're on main
+    changed_dirs="$(cd plugins && ls -d intentional*/ | jq -R -s -c 'split("\n") | map(select(length > 0))')"
+    echo $changed_dirs
+else
+    # we're on a PR
     changed_main_dirs=$(git diff --name-only main...HEAD && git diff --name-only --cached | grep '^intentional' | awk -F'/' '{print $1}' | sort -u | jq -R . | jq -s .)
     changed_plugins_dirs=$(git diff --name-only main...HEAD && git diff --name-only --cached | grep '^plugins/intentional' | awk -F'/' '{print $1"/"$2}' | sort -u | jq -R . | jq -s .)
     changed_dirs=$(jq -c -n --argjson list1 "$changed_main_dirs" --argjson list2 "$changed_plugins_dirs" '$list1 + $list2')
-    echo $changed_dirs
-else
-    changed_dirs="$(cd plugins && ls -d intentional*/ | jq -R -s -c 'split("\n") | map(select(length > 0))')"
     echo $changed_dirs
 fi
